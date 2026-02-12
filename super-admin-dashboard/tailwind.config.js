dcf5e7df34c7599d4aee4656194eed8f2527e2c6
Akshay-react <akshayraj.k@ziyaacademy.co.in>
2026-02-12 18:47:56 +0530

    Setup project structure for super admin dashboard

--- super-admin-dashboard/tailwind.config.js
+++ super-admin-dashboard/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+                arimo: ["Arimo", "sans-serif"],
+
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
